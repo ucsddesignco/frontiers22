50c44d7a539a3d2d1b69a293f5a296b64c8d5020
Shawn Kim <sjk002@ucsd.edu>
2022-04-22 17:13:20 -0700

    horizontal scroll

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Landing from './components/Landing';
+import Skeleton from './components/Skeleton';
 
 function App() {
   return (
     <div className="App">
-      <Landing />
+      <Skeleton />
     </div>
   );
 }
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -7,7 +7,7 @@ const Landing = () => {
   return (
     <div>
       <h1>Hello World</h1>
-      <p>This is a paragraph</p>
+      <p>Design Frontiers</p>
     </div>
   )
 }
