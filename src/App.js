8759f9d0401c35427a5c8fe6c36e104b531dceae
Meshach Adoe <madoe@ucsd.edu>
2022-04-23 21:38:14 -0700

    Add timeline component and reorganize h-scroll logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-import Landing from './components/Landing';
-import Skeleton from './components/Skeleton';
+import React, { useEffect } from 'react';
+import './styles/main.scss';
+
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+import Timeline from './components/Timeline';
 
 function App() {
+  gsap.registerPlugin(ScrollTrigger);
+  useEffect(() => {
+    const container = document.getElementById("container");
+    const sec = document.getElementById("section");
+    const tl1 = gsap.timeline();
+    const tl2 = gsap.timeline();
+
+    tl1.to(container, {
+      x: () => -(container.scrollWidth - document.documentElement.clientWidth) + "px",
+      ease: "none",
+      scrollTrigger: {
+        trigger: container,
+        invalidateOnRefresh: true,
+        pin: true,
+        scrub: 1,
+        end: () => "+=" + container.offsetWidth,
+      }
+    })
+
+    tl2.to(sec, {
+        scaleX: 0.5,
+        scaleY: 0.5,
+        transformOrigin: 'left top',
+        ease: "none",
+        scrollTrigger: {
+            trigger: container,
+            invalidateOnRefresh: true,
+            scrub: 1,
+            start: 0,
+            end: '+=500'
+        }
+    })
+  })
+
   return (
     <div className="App">
-      <Skeleton />
+      <div className="desktop-container">
+        <div className="module green" id="section" ></div>
+        <div id="container">
+          <div className="module yellow"></div>
+          <div className="module purple"></div>
+          <Timeline />
+          <div className="module blue"></div>
+          <div className="module red"></div>
+        </div>
+      </div>
+      <div className="mobile-container">
+        <Timeline />
+      </div>
     </div>
   );
 }
--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -2,9 +2,62 @@ import React from 'react'
 
 import './style.scss';
 
+const events = [
+  {
+    time: "10 AM",
+    header: "Event Kickoff",
+    description: "Registration & Icebreakers, Opening Remarks, Brief Introduction, Keynote Speaker"
+  },
+  {
+    time: "11 AM",
+    header: "Sprint Starts",
+    description: "Design Sprint, Lunch"
+  },
+  {
+    time: "2:30 PM",
+    header: "Sprint Ends",
+    description: "Presentations Begin, Judging Begins "
+  },
+  {
+    time: "3:30 PM",
+    header: "Closing Ceremony",
+    description: "3 Winners Are Announced, Prize Distribution, Networking Session"
+  },
+  {
+    time: "4 PM",
+    header: "Event Ends"
+  }
+]
+
 const Timeline = () => {
   return (
-    <div>Timeline</div>
+    <div className="timeline">
+      <h2 className="timeline__header">Timeline</h2>
+      <h2>Thursday, May 15</h2>
+      <ul className="timeline__table">
+        {
+          events.map((event) => (
+            <li className="timeline__row">
+              <div className="timeline__row__time">
+                <span>{event.time}</span>
+              </div>
+              <div className="timeline__row__detail">
+                <div className="timeline__row__detail__header">
+                  <h3>{event.header}</h3>
+                </div>
+                {
+                  event.description && 
+                  <div className="timeline__row__detail__info">
+                    <p>{event.description}</p>
+                  </div>
+                }
+              </div>
+            </li>
+          ))
+        }
+        
+      </ul>
+    </div>
   )
 }
 
